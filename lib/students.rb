8d45c892b86e0e059daf11ff681ff7958009e1cf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-05 02:17:07 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,28 +1,30 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  SELECT MAX(column_name)
-FROM table_name
-WHERE condition; 
+  "SELECT MAX(column_name)
+  FROM table_name
+  WHERE condition;"
 end
 
 def lowest_student_gpa
-  SELECT MIN(column_name)
-FROM table_name
-WHERE condition; 
+  "SELECT MIN(column_name)
+  FROM table_name
+  WHERE condition;"
 end
 
-  def average_student_gpa
-  SELECT AVG(column_name)
+def average_student_gpa
+  "SELECT AVG(column_name)
   FROM table_name
-  WHERE condition;
+  WHERE condition;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(Quantity)
+  FROM OrderDetails;"
 end
 
 def average_gpa_for_9th_grade
-  SELECT AVG(column_name)
+  "SELECT AVG(column_name)
   FROM table_name
-  WHERE condition;
+  WHERE condition;"
 end
