1840b3ca0718119e22c1ec9fbdf87038c0eb2100
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-05 02:21:36 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -2,29 +2,29 @@
 
 def highest_student_gpa
   "SELECT MAX(GPA)
-  FROM table_name
+  FROM STUDENTS
   WHERE condition;"
 end
 
 def lowest_student_gpa
   "SELECT MIN(GPA)
-  FROM table_name
+  FROM STUDENTS
   WHERE condition;"
 end
 
 def average_student_gpa
   "SELECT AVG(GPA)
-  FROM table_name
+  FROM STUDENTS
   WHERE condition;"
 end
 
 def total_tardies_for_all_students
   "SELECT SUM(QUANTITY)
-  FROM OrderDetails;"
+  FROM STUDENTS;"
 end
 
 def average_gpa_for_9th_grade
   "SELECT AVG(GPA)
-  FROM table_name
+  FROM STUDENTS
   WHERE condition;"
 end
