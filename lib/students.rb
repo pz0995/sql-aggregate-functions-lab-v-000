d67702468e3add037f120406c25e887a89fddedf
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-08-05 02:27:44 +0000

    Done.

--- lib/students.rb
+++ lib/students.rb
@@ -1,16 +1,30 @@
-## Code your solution below. Note that your SQL queries should be in quotation marks. 
+## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
+  "SELECT MAX(GPA)
+  FROM STUDENTS;"
+  # WHERE condition;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(GPA)
+  FROM STUDENTS;"
+  # WHERE condition;"
 end
 
 def average_student_gpa
+  "SELECT AVG(GPA)
+  FROM STUDENTS;"
+  # WHERE condition;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(TARDIES)
+  FROM STUDENTS;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(GPA)
+  FROM STUDENTS
+  WHERE Grade=9;"
 end
